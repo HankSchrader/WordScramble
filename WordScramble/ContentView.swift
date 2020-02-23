0ee034c20afbc213a75d5118e473a362b957afe2
Erik Mikac <mikacew@gmail.com>
2020-02-23 15:16:12 -0500

    Added rescamble button. Added score.

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -16,6 +16,8 @@ struct ContentView: View {
     @State private var errorTitle = ""
     @State private var errorMessage = ""
     @State private var errorShowing = false
+    @State private var allWords = [String]()
+    @State private var score = 0
 
      var body: some View {
         NavigationView {
@@ -26,7 +28,10 @@ struct ContentView: View {
                     Image(systemName: "\($0.count).circle")
                     Text($0)
                 }
-            }.navigationBarTitle(rootWord)
+                Text("Score: \(score)").foregroundColor(score > 0 ? .green : .red)
+            }.navigationBarTitle(rootWord).navigationBarItems(trailing: Button(action: rescramble) {
+                Text("Rescramble!")
+            })
             .onAppear(perform: startGame)
                 .alert(isPresented: $errorShowing) {
                     Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
@@ -41,26 +46,52 @@ struct ContentView: View {
             }
             guard isOriginal(word: answer) else {
                 wordError(title: "Word used already!", message: "Be more original!")
+                score -= answer.count
                 return
             }
             guard isPossible(word: answer) else {
                 wordError(title: "Word not recognized", message: "You can't just make up words, ya know.")
+                score -= answer.count
                 return
             }
             
-            guard isRealWord(word: answer) else {
+            guard isReal(word: answer) else {
                 wordError(title: "Word not possible", message: "That isn't a real word.")
+                score -= answer.count
+                return
+            }
+            
+            guard isLessThanThree(word: answer) else {
+                wordError(title: "Word too small.", message: "Word must be more than three letters.")
+                score -= answer.count
+                return
+            }
+            
+            guard isSameWord(word: answer) else {
+                wordError(title: "Same word.", message: "The word must be different from the root word.")
+                  score -= answer.count
                 return
             }
             usedWords.insert(answer, at: 0)
             newWord = ""
+            score += answer.count
         }
+    
+    func rescramble() {
+        if let newWord = allWords.randomElement() {
+            rootWord = newWord
+            usedWords = []
+            score = 0
+        } else {
+            fatalError("Word could not be rescrambled!")
+        }
+    }
     
     func startGame() {
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try?
                 String(contentsOf: startWordsURL) {
-                let allWords = startWords.components(separatedBy: "\n")
+                allWords = startWords.components(separatedBy: "\n")
                 rootWord = allWords.randomElement() ?? "silkworm"
                 return
             }
@@ -82,11 +113,19 @@ struct ContentView: View {
             }
             return true
         }
-    func isRealWord(word: String) -> Bool {
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRanged = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         return misspelledRanged.location == NSNotFound
+    }
+    
+    func isLessThanThree(word: String) -> Bool {
+        return word.count > 3
+    }
+    
+    func isSameWord(word: String) -> Bool {
+        return word != self.rootWord
     }
     
     func wordError(title: String, message: String) {
