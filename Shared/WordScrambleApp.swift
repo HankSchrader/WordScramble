2238d5f30353b1330042ca041ea596839a072525
Erik Mikac <mikacew@gmail.com>
2021-03-03 19:51:38 -0500

    Initial Commit

---
+++ Shared/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  Shared
+//
+//  Created by Erik Mikac on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
